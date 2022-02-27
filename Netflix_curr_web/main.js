48683364ef815b6112dd9f0738192f22ac382422
dygreen <95523625+dygreen@users.noreply.github.com>
2022-02-27 22:09:52 +0900

    Add files via upload

--- Netflix_curr_web/main.js
+++ Netflix_curr_web/main.js
@@ -1,8 +1,17 @@
-'use strict';
-const toggle = document.querySelector('.navbar__toggle');
-const dropdownMenu = document.querySelector('.dropdown__menu');
+"use strict";
+const toggle = document.querySelector(".navbar__toggle");
+const dropdownMenu = document.querySelector(".dropdown__menu");
 
-toggle.addEventListener('click', () => {
-    toggle.classList.toggle('clicked');
-    dropdownMenu.classList.toggle('active');
+toggle.addEventListener("click", () => {
+  toggle.classList.toggle("clicked");
+  dropdownMenu.classList.toggle("active");
+});
+
+// scroll을 내리면 navbar background가 black으로 바뀌는
+$(window).on("scroll", () => {
+  if ($(window).scrollTop() > 100) {
+    $(".navbar").addClass("navbar__black");
+  } else {
+    $(".navbar").removeClass("navbar__black");
+  }
 });
